fb16b7b35dc455c74f35abd7c3952bb2ca2ff2d9
Aditya Kulkarni <adityakul0314@gmail.com>
2023-05-20 16:36:39 -0700

    Feature/home (#3)
    * made small changes to home page
    
    * added pressEnter hook

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
+import { useKeyPress } from "../../hooks";
 
 function Home() {
   const [text, setText] = useState<string>("");
-  console.log(text);
+  const isEnterPressed = useKeyPress("Enter");
+
+  if (isEnterPressed) {
+    console.log(text);
+  }
+
   return (
     <div className="absolute top-20 flex w-full justify-center">
       <div className="mt-2 w-[80%]">
         <input
           type="text"
-          className="h-10 w-full flex-1 rounded-xl border border-gray-400 bg-white px-5 text-gray-400 opacity-30 shadow focus:border-0 focus:opacity-100 focus:ring-gray-500"
+          className="h-10 w-full flex-1 rounded-xl border border-gray-400 bg-white px-5 text-gray-700 opacity-30 shadow outline-none ring-0 hover:opacity-100 focus:border-0 focus:opacity-100 focus:ring-0"
           value={text}
           autoComplete="false"
           placeholder="Ask a question"

daf575e631df319a3eeab0b98a77c3ac3c186e14
Aditya Kulkarni <adityakul0314@gmail.com>
2023-05-20 18:22:25 -0700

    Feature/axios searchbar (#5)
    Axios searchbar

---
+++ src/services/BaseService.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const baseURL = "https://stormhacks-2023-t5ql44yo2q-uw.a.run.app/";
+
+const axiosInstance = axios.create({
+  baseURL,
+  withCredentials: true,
+  headers: {
+    // 'Access-Control-Allow-Origin': '*',
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.response.use(undefined, (error) => {
+  if (error?.response?.status !== 401) {
+    return Promise.reject(error);
+  }
+});
+
+export default axiosInstance;

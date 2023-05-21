75be6c7ce555333e42ce5e913c9893e29431158a
aetaati <amiropt57@gmail.com>
2023-05-20 18:11:22 -0700

    Add and style Help and Home components

--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -1,5 +1,14 @@
 function Help() {
-    return <div className="absolute top-20 text-white">Help</div>;
-  }
-  
-  export default Help;
+  return (
+      // <div className="absolute top-20 text-white">Help</div>
+      <div className="absolute opacity-80 hover:opacity-100 top-20 flex justify-center w-full">
+        <div className="bg-white p-8 rounded-lg justify-center w-[70%]">
+
+          <h1 className="text-2xl font-bold text-center">Help</h1>
+          <p className="text-gray-700 text-center">This is the information about the web app's features...</p>
+        </div>
+      </div>
+      )
+}
+
+export default Help;

4e6eba7e8e3d6a36a96e20295c76d6bac15b34db
aetaati <amiropt57@gmail.com>
2023-05-21 11:47:58 -0700

    Add help info

--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -5,7 +5,29 @@ function Help() {
         <div className="bg-white p-8 rounded-lg justify-center w-[70%]">
 
           <h1 className="text-2xl font-bold text-center">Help</h1>
-          <p className="text-gray-700 text-center">This is the information about the web app's features...</p>
+          <div className="help-tab">
+          <h5 className="font-bold text-center" >Welcome to the Help tab of our website!</h5>
+          <p>Here, we provide assistance and guidance on how to make the most of our mountain exploration features. Discover the wonders of the mountains with these helpful features:</p>
+          <ul>
+            <li>
+              <strong>Search for Mountain Names:</strong> Use the search bar to find specific mountains you're interested in. Simply type in the name of the mountain, and we'll help you explore it in detail.
+            </li>
+            <li>
+              <strong>Mountain Images and Information:</strong> Once you've found a mountain, you'll be able to view captivating images of it along with essential information. Immerse yourself in the beauty and grandeur of the mountains through stunning visuals.
+            </li>
+            <li>
+              <strong>Mountain Topology:</strong> Dive deeper into your favorite mountains by exploring their topography. Get a comprehensive understanding of the landscape, elevation, and geological features that make each mountain unique.
+            </li>
+            <li>
+              <strong>3D Images of Inhabiting Animals:</strong> Witness the diversity of wildlife that inhabits these majestic mountains. Experience an interactive 3D showcase that brings the animals to life. Discover their natural habitats and learn about their characteristics and behaviors.
+            </li>
+            <li>
+              <strong>Ratings and Reviews:</strong> Curious about what others think of a particular mountain? Check out the ratings and reviews section. See how users have rated their experiences and leave your own rating to share your thoughts and help fellow mountain enthusiasts make informed decisions.
+            </li>
+          </ul>
+          <p>We hope these features enhance your mountain exploration journey. If you have any further questions or need assistance, feel free to reach out. Happy mountain exploring!</p>
+        </div>
+
         </div>
       </div>
       )
